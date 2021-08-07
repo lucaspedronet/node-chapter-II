f46c88526b9b34109ce7571b896bdcb0b68a27c8
lucaspedronet <lucaspedrolbg@gmail.com>
2021-08-07 00:07:01 -0300

    [Stream] aplicando stream csv

--- src/modules/cards/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cards/useCases/importCategory/importCategoryUseCase.ts
@@ -1,6 +1,20 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
-  execute(file: any): void {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    // criando um stream de leitura do arquivo
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    // pipe() pega esse pedaço de arquivo q foi lido e passa para parseFile
+    stream.pipe(parseFile);
+
+    // o parseFile.on() vai ler linha por linha desse arquivo csv
+    parseFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
 
